f89c00c1be0cb47b0ce7f391e6ba8d47ac104d7d
Hemel Parody <hemelparody@hotmail.com>
2024-05-04 12:47:02 -0500

    Feat: home 2

--- src/components/Header/navigationList.js
+++ src/components/Header/navigationList.js
@@ -1,7 +1,7 @@
 import { faStore, faHouseChimney } from "@fortawesome/free-solid-svg-icons";
 
 const tabsList = [
-  { redirectRoute: "/home", label: "HOME", icon: faHouseChimney },
+  { redirectRoute: "/home", label: "HOME 2", icon: faHouseChimney },
   { redirectRoute: "/products", label: "PRODUCTS", icon: faStore },
 ];
 
